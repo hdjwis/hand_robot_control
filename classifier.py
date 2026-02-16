10920a9c1a5ca2bf9a4503cddcfab06dca8a8ebb
hdjwis <adarshyraju@gmail.com>
2026-02-15 20:44:58 -0600

    created first attempt at fastapi classifier

---
+++ classifier.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import os
+from fastai.vision.all import *
+from pathlib import Path
+import matplotlib.pyplot as plt 
+
+def label_func(f): return f[0] + f[1]
+
+path = Path.cwd() / 'data'
+files = get_image_files(path)
+dls = ImageDataLoaders.from_name_func(path, files, label_func, item_tfms=Resize(224))
+learn = vision_learner(dls, resnet34, metrics=error_rate)
+learn.fine_tune(2)
+learn.show_results()
+plt.show()
